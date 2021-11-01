f29f77b8fb0d74aa051b0e74e066a6e585fe3785
NovozhilovAY <sasha.n191@yandex.ru>
2021-11-01 12:28:47 +0300

    added ex4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,14 @@ package com.company;
 import com.company.chapter1.Ex1;
 import com.company.chapter1.Ex2;
 import com.company.chapter1.Ex3;
+import com.company.chapter1.Ex4;
 
 public class Main {
     public static void main(String[] args) {
         //Ex1.run();
         //Ex2.run();
-        Ex3.run();
+        //Ex3.run();
+        Ex4.run();
 
 
     }
---
+++ src/com/company/chapter1/Ex4.java
@@ -0,0 +1,8 @@
+package com.company.chapter1;
+
+public class Ex4 {
+    public static void run() {
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+    }
+}
