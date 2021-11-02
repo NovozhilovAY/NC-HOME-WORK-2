64b60adc37b3e80b92521d5ef284a00bd50bc440
NovozhilovAY <sasha.n191@yandex.ru>
2021-11-02 11:58:02 +0300

    added Point class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import com.company.chapter1.*;
+import com.company.chapter2.Point;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +10,10 @@ public class Main {
         //Ex3.run();
         //Ex4.run();
         //Ex6.run();
-        Ex13.run();
+        //Ex13.run();
+        Point p = new Point(3,4).translate(1, 3).scale(0.5);
+        System.out.println(p);
+
 
 
 
---
+++ src/com/company/chapter2/Point.java
@@ -0,0 +1,38 @@
+package com.company.chapter2;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point() {
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public Point translate(double dx, double dy) {
+        return new Point(x + dx, y + dy);
+    }
+
+    public Point scale(double coefficient) {
+        return new Point(x * coefficient, y * coefficient);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
